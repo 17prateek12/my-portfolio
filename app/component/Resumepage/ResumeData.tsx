b28c0f33b09357816a82bd5db3ae8745dc93d0ed
17prateek12 <88935432+17prateek12@users.noreply.github.com>
2024-03-20 05:29:44 +0530

    Update ResumeData.tsx

--- app/component/Resumepage/ResumeData.tsx
+++ app/component/Resumepage/ResumeData.tsx
@@ -9,8 +9,8 @@ const ResumeData=[
              'Developed a false news detection system utilising machine learning models and natural language processing techniques, incorporating four distinct combination models, and successfully deployed the best model on Flask-based web page.',
              'Achieved an accuracy rate of up to 89.91 percent, demonstrating expertise in model web deployment, handle data analysis, and data visualization.'
         ]
-    },
-]
+    }
+];
 export default ResumeData;
 
 
