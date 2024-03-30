d94648c8da6a9bd9004001c76cf951a4d41e04c7
17prateek12 <88935432+17prateek12@users.noreply.github.com>
2024-03-30 21:04:27 +0530

    second commit

--- app/component/projectcompo/ProjectData.tsx
+++ app/component/projectcompo/ProjectData.tsx
@@ -1,38 +1,49 @@
-const ProjectData=[
+const ProjectData = [
     {
-        id:1,
-        title:'STOCK PREDICTION DASHBOARD',
-        tech:'React.JS, Tailwind CSS, FinHub API, Flask, Neural Network, LSTM, SVR, Scikit-learn',
-        point2:[
+        id: 1,
+        title: <a href="https://weather-app-tau-indol.vercel.app/" target="_blank" style={{ textDecoration: 'none' ,color:'black' }}>WEATHER APP</a>,
+        tech: 'React.JS',
+        point2: [
+            'Developed a Weather Dashboard application using React, providing users with current weather information and a 5-day forecast for specified locations.',
+            'Utilized React Context API and Hooks for efficient state management and component interaction.',
+            'Integrated OpenWeatherMap API to fetch weather data, and utilized Recharts library for graphical representation of forecast data.',
+        ],
+        git: <a href="https://github.com/17prateek12/Weather_app" target="_blank" style={{ textDecoration: 'none' }}>GITHUB</a>
+    },
+    {
+        id: 2,
+        title: 'STOCK PREDICTION DASHBOARD',
+        tech: 'React.JS, Tailwind CSS, FinHub API, Flask, Neural Network, LSTM, SVR, Scikit-learn',
+        point2: [
             'Developed a stock price prediction dashboard using LSTM and SVR on different datasets (5 and 10 years).',
             'Displayed opening price graphs for various timeframes (e.g., 1 day, 1 month), company information and expected stock price predictions.',
             'Integrated third-party APIs (FinHub API) to extract up to 3 years of data for the graph created by React for designing the frontend of the application and Tailwind CSS for styling the application.',
             'Deployed LSTM model on the Flask API, predicting next day opening price with 97% and setup the frontend for relay prediction and dark mode.',
         ],
-        git:<a href ="https://github.com/17prateek12/Stock_prediction"  target="_blank"  style={{textDecoration:'none'}}>GITHUB</a>
+        git: <a href="https://github.com/17prateek12/Stock_prediction" target="_blank" style={{ textDecoration: 'none' }}>GITHUB</a>
     },
     {
-        id:2,
-        title:'PHOTO GALLERY',
-        tech:'React.JS, Material-UI, CSS, Django REST, MySQL',
-        point2:[
+        id: 3,
+        title: 'PHOTO GALLERY',
+        tech: 'React.JS, Material-UI, CSS, Django REST, MySQL',
+        point2: [
             'Created a Full-stack responsive photo gallery with feature of uploading photo, category creation, descriptions (up to 500 words).',
             'Implemented theme customization and sorting of photos on two options (on the basis of name and time of creation).',
             'Strengthened the application with crud operations on photos (editing, updating, deleting) and added two more advanced features like category-based deletion and filtering.'
         ],
-        git:<a href ="https://github.com/17prateek12/Photo-gallery" target="_blank" style={{textDecoration:'none'}}>GITHUB</a>
-   },
+        git: <a href="https://github.com/17prateek12/Photo-gallery" target="_blank" style={{ textDecoration: 'none' }}>GITHUB</a>
+    },
 
-   {
-   id:4,
-    title:'AUTONOMOUS SMART CAR',
-    tech:'Raspberry Pi, Neural Networks, Image Processing, Sensors, Google Map API, JavaScript',
-    point2:[
-        'Designed a self-driving car demonstrating capabilities in lane detection, curve-based turning, obstacle avoidance, and real-time location of the car using a GPS sensor (Neo 6M).',
-        'Compared two algorithms for lane detection using Raspberry Pi, camera, and OpenCV library, achieving CNN accuracy of approximately 77%.',
-    ],
-    git:<a href ="https://github.com/17prateek12/autonomous-smart-car" target="_blank"  style={{textDecoration:'none'}}>GITHUB</a>
-},
+    {
+        id: 4,
+        title: 'AUTONOMOUS SMART CAR',
+        tech: 'Raspberry Pi, Neural Networks, Image Processing, Sensors, Google Map API, JavaScript',
+        point2: [
+            'Designed a self-driving car demonstrating capabilities in lane detection, curve-based turning, obstacle avoidance, and real-time location of the car using a GPS sensor (Neo 6M).',
+            'Compared two algorithms for lane detection using Raspberry Pi, camera, and OpenCV library, achieving CNN accuracy of approximately 77%.',
+        ],
+        git: <a href="https://github.com/17prateek12/autonomous-smart-car" target="_blank" style={{ textDecoration: 'none' }}>GITHUB</a>
+    },
 ];
 
 export default ProjectData;
